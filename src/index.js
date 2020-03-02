b0900cc6956e1e7804ac6838ae289ddfe07fc71a
KateProtasevich <KrishniovaKate@mail.ru>
2020-03-02 17:27:40 +0300

    init: start array

--- src/index.js
+++ src/index.js
@@ -1,12 +1,36 @@
 
 exports.min = function min (array) {
-  return 0;
+  if (arguments.length === 0 ) {
+    return 0;
+  }
+  let minNumber = 0;
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] < minNumber) {
+      minNumber = array[i];
+    }
+  }
+
+  return minNumber;
 }
 
 exports.max = function max (array) {
+  if (arguments.length === 0 ) {
   return 0;
+}
+let maxNumber = 0;
+for (let i = 0; i < array.length; i++) {
+  if (array[i] > maxNumber) {
+    maxNumber = array[i];
+  }
+}
+
+return maxNumber;
 }
 
 exports.avg = function avg (array) {
-  return 0;
+  if (arguments.length === 0 || array.length ===0) {
+    return 0;
+  }
+
+  return array.reduce((sum, elem) => sum + elem)/ array.length;
 }
